38be5d99b27767dc756c8df2f3cbbab457ef0b0f
s9badoer <s9badoer@1fa67fd9-a42d-4e50-ecb6-14fb473a4415>
2011-10-23 10:32:34 +0000

    classes added for tokens that need attributes (Identifiers, Numbers)

---
+++ src/scanner/Identifier.h
@@ -0,0 +1,27 @@
+/*
+ * Identifier.h
+ *
+ *  Created on: 23.10.2011
+ *      Author: barbara
+ */
+
+#ifndef IDENTIFIER_H_
+#define IDENTIFIER_H_
+
+#include <string>
+
+#include "Token.h"
+
+using namespace std;
+
+class Identifier: public Token {
+public:
+	Identifier(string s);
+	virtual ~Identifier();
+	string getIdentifier();
+
+private:
+	string identifier;
+};
+
+#endif /* IDENTIFIER_H_ */
